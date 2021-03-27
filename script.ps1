6dfcc93d1df3c723f437e4af4db840a7b0abb6d8
Dipaditya Das <DipadityaDas@gmail.com>
2021-03-27 11:49:42 +0530

    Modified Script File

--- script.ps1
+++ script.ps1
@@ -47,4 +47,5 @@ Write-Host "] Running with Admin Rights" -ForegroundColor Green
 # Changing the value of HypervisiorLauchType in Boot Configuration Data (BCD) file 
 $bcdeditvalue = bcdedit /set hypervisorlaunchtype  Off
 If ($bcdeditvalue -eq "The operation completed successfully.") {
+	Write-Host "[" -ForegroundColor Green -NoNewline
 }
